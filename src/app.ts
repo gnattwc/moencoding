14371a43e82d6082573ffbdf4009b4bd9e610602
Chris Tang <33366630+gnattwc@users.noreply.github.com>
2023-03-10 15:46:34 -0800

    webpack bundle

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
-// import { TwoWaterJugRiddle, JugSolution } from "./TwoWaterJugRiddle";
-// import { sampleSolver } from "./WaterJugSolver";
+import { TwoWaterJugRiddle, JugSolution, JugAction } from "./TwoWaterJugRiddle";
+import { bfsSolver } from "./WaterJugSolver";
 
-const root: HTMLElement = document.getElementById("root")!;
-root.textContent = "hi root";
+// const root: HTMLElement = document.getElementById("root")!;
+// root.textContent = "hi root";
 
-// const riddle = new TwoWaterJugRiddle(sampleSolver);
-// const solution: JugSolution = riddle.solveRiddle(4, 3, 2);
+const riddle = new TwoWaterJugRiddle(bfsSolver);
+const solution: JugSolution = riddle.solveRiddle(4, 3, 2);
 
-// const list = document.getElementById("outputList")!;
-// if (solution.length > 0) {
-//   solution.forEach((s) => {
-//     const li = document.createElement("li");
-//     list.appendChild(document.createTextNode(s.toString()));
-//   });
-// } else {
-//   const li = document.createElement("li");
-//   list.appendChild(document.createTextNode("No Solution"));
-// }
-
-// export {};
+const list: HTMLElement = document.getElementById("outputList")!;
+if (solution.length > 0) {
+  solution.forEach((s,i) => {
+    const li = document.createElement("li");
+    const str:string = 'action='+JugAction[s.action]+ s.state;
+    list.appendChild(document.createTextNode(str));
+  });
+} else {
+  const li = document.createElement("li");
+  list.appendChild(document.createTextNode("No Solution"));
+}
