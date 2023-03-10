be510099fa8109a8e5b36286822c1172f17c49d8
Chris Tang <33366630+gnattwc@users.noreply.github.com>
2023-03-10 09:29:19 -0800

    initial check-in

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+// import { TwoWaterJugRiddle, JugSolution } from "./TwoWaterJugRiddle";
+// import { sampleSolver } from "./WaterJugSolver";
+
+const root: HTMLElement = document.getElementById("root")!;
+root.textContent = "hi root";
+
+// const riddle = new TwoWaterJugRiddle(sampleSolver);
+// const solution: JugSolution = riddle.solveRiddle(4, 3, 2);
+
+// const list = document.getElementById("outputList")!;
+// if (solution.length > 0) {
+//   solution.forEach((s) => {
+//     const li = document.createElement("li");
+//     list.appendChild(document.createTextNode(s.toString()));
+//   });
+// } else {
+//   const li = document.createElement("li");
+//   list.appendChild(document.createTextNode("No Solution"));
+// }
+
+// export {};
