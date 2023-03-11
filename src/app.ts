6830b0fa77ff18b5369c4147759730a4a29397c6
Chris Tang <33366630+gnattwc@users.noreply.github.com>
2023-03-11 10:12:06 -0800

    animation steps

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { TwoWaterJugRiddle, JugSolution, JugAction } from "./TwoWaterJugRiddle";
 import { bfsSolver } from "./WaterJugSolver";
 
-let btn = <HTMLButtonElement>document.getElementById("run");
+const btn = <HTMLButtonElement>document.getElementById("run");
 btn.addEventListener("click", (e: Event) => solveRiddle());
 
 function solveRiddle() {
@@ -13,8 +13,12 @@ function solveRiddle() {
 
   console.log(jug1.value, jug2.value, aim.value);
   if (!jug1.value || !jug2.value || !aim.value) {
+    alert("Please enter all values for X,Y,Z");
     return;
   }
+
+  const form = <HTMLFormElement>document.getElementById("xyzForm");
+  form.style.visibility = "hidden";
 
   const riddle = new TwoWaterJugRiddle(bfsSolver);
   const solution: JugSolution = riddle.solveRiddle(
@@ -24,7 +28,58 @@ function solveRiddle() {
   );
 
   displayOutput2(solution);
+  runAnimation(solution, 0, +jug1.value, +jug2.value, +aim.value);
 }
+
+function runAnimation(
+  solution: JugSolution,
+  i: number,
+  jug1: number,
+  jug2: number,
+  aim: number
+) {
+  const leftGlass = <HTMLDivElement>document.getElementById("leftGlass");
+  const rightGlass = <HTMLDivElement>document.getElementById("rightGlass");
+
+  const leftFrom = i > 0 ? (solution[i - 1].state[0] / jug1) * 100 : 0;
+  const rightFrom = i > 0 ? (solution[i - 1].state[1] / jug2) * 100 : 0;
+  const leftTo = (solution[i].state[0] / jug1) * 100;
+  const rightTo = (solution[i].state[1] / jug2) * 100;
+  const aniDur = 1600;
+
+  leftGlass.style.setProperty("--leftFillFrom", `-${leftFrom}px`);
+  leftGlass.style.setProperty("--leftFillTo", `-${leftTo}px`);
+  leftGlass.style.setProperty("--leftAniDur", `${aniDur}ms`);
+  rightGlass.style.setProperty("--rightFillFrom", `-${rightFrom}px`);
+  rightGlass.style.setProperty("--rightFillTo", `-${rightTo}px`);
+  rightGlass.style.setProperty("--rightAniDur", `${aniDur}ms`);
+
+  leftGlass.classList.add("leftAni");
+  rightGlass.classList.add("rightAni");
+
+  if (i < solution.length - 1) {
+    setTimeout(() => {
+      resetAnimation(solution, i, jug1, jug2, aim);
+    }, 1600);
+  }
+}
+
+function resetAnimation(
+    solution: JugSolution,
+    i: number,
+    jug1: number,
+    jug2: number,
+    aim: number
+  ) {
+    const leftGlass = <HTMLDivElement>document.getElementById("leftGlass");
+    const rightGlass = <HTMLDivElement>document.getElementById("rightGlass");
+    leftGlass.classList.remove("leftAni");
+    rightGlass.classList.remove("rightAni");
+
+    setTimeout(() => {
+      runAnimation(solution, i + 1, jug1, jug2, aim);
+    }, 500); 
+  }
 
 // display output
 function displayOutput2(solution: JugSolution) {
@@ -33,7 +88,7 @@ function displayOutput2(solution: JugSolution) {
     table.style.visibility = "visible";
     solution.forEach((s, i) => {
       // Create an empty <tr> element and add it to the 1st position of the table:
-      var row = table.insertRow(i+1);
+      var row = table.insertRow(i + 1);
 
       // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
       var cell1 = row.insertCell(0);
@@ -49,6 +104,8 @@ function displayOutput2(solution: JugSolution) {
     const h2 = <HTMLHeadingElement>document.getElementById("noSolution");
     h2.style.visibility = "visible";
   }
+  const form = <HTMLFormElement>document.getElementById("xyzForm");
+  form.style.visibility = "visible";
 }
 
 function clearOutput2() {
