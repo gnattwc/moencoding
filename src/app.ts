e00db41ecde6ea066e02f4dffc6f1b24fcc50609
Chris Tang <33366630+gnattwc@users.noreply.github.com>
2023-03-10 18:03:11 -0800

    basic working UI

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,38 @@ import { bfsSolver } from "./WaterJugSolver";
 // const root: HTMLElement = document.getElementById("root")!;
 // root.textContent = "hi root";
 
-const riddle = new TwoWaterJugRiddle(bfsSolver);
-const solution: JugSolution = riddle.solveRiddle(4, 3, 2);
+let btn = <HTMLButtonElement>document.getElementById("run");
+btn.addEventListener("click", (e: Event) => solveRiddle());
 
-const list: HTMLElement = document.getElementById("outputList")!;
-if (solution.length > 0) {
-  solution.forEach((s,i) => {
+function solveRiddle() {
+  //   alert("haha");
+
+  const jug1 = <HTMLInputElement>document.getElementById("leftJug");
+  const jug2 = <HTMLInputElement>document.getElementById("rightJug");
+  const aim = <HTMLInputElement>document.getElementById("aim");
+
+  console.log(jug1.value, jug2.value, aim.value);
+  if (!jug1.value || !jug2.value || !aim.value) {
+    return;
+  }
+
+  const riddle = new TwoWaterJugRiddle(bfsSolver);
+  const solution: JugSolution = riddle.solveRiddle(
+    +jug1.value,
+    +jug2.value,
+    +aim.value
+  );
+
+  const list: HTMLElement = document.getElementById("solutionList")!;
+  if (solution.length > 0) {
+    solution.forEach((s, i) => {
+      const li = document.createElement("li");
+      const str: string = "action=" + JugAction[s.action] + '   ' + s.state;
+      li.innerText = str;
+      list.appendChild(li);
+    });
+  } else {
     const li = document.createElement("li");
-    const str:string = 'action='+JugAction[s.action]+ s.state;
-    list.appendChild(document.createTextNode(str));
-  });
-} else {
-  const li = document.createElement("li");
-  list.appendChild(document.createTextNode("No Solution"));
+    list.appendChild(document.createTextNode("No Solution"));
+  }
 }
