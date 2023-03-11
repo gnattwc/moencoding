22bc8298b70cb2c61ba1847d9f3c4da2a2028609
Chris Tang <33366630+gnattwc@users.noreply.github.com>
2023-03-11 10:25:33 -0800

    sync table hightlight

--- src/app.ts
+++ src/app.ts
@@ -57,29 +57,35 @@ function runAnimation(
   leftGlass.classList.add("leftAni");
   rightGlass.classList.add("rightAni");
 
-  if (i < solution.length - 1) {
-    setTimeout(() => {
-      resetAnimation(solution, i, jug1, jug2, aim);
-    }, 1600);
-  }
+  const table = <HTMLTableElement>document.getElementById("solutionTable")!;
+  table.rows[i + 1].classList.add("highlight");
+
+  setTimeout(() => {
+    resetAnimation(solution, i, jug1, jug2, aim);
+  }, 1600);
 }
 
 function resetAnimation(
-    solution: JugSolution,
-    i: number,
-    jug1: number,
-    jug2: number,
-    aim: number
-  ) {
-    const leftGlass = <HTMLDivElement>document.getElementById("leftGlass");
-    const rightGlass = <HTMLDivElement>document.getElementById("rightGlass");
-    leftGlass.classList.remove("leftAni");
-    rightGlass.classList.remove("rightAni");
+  solution: JugSolution,
+  i: number,
+  jug1: number,
+  jug2: number,
+  aim: number
+) {
+  const leftGlass = <HTMLDivElement>document.getElementById("leftGlass");
+  const rightGlass = <HTMLDivElement>document.getElementById("rightGlass");
+  leftGlass.classList.remove("leftAni");
+  rightGlass.classList.remove("rightAni");
 
+  const table = <HTMLTableElement>document.getElementById("solutionTable")!;
+  table.rows[i + 1].classList.remove("highlight");
+
+  if (i < solution.length - 1) {
     setTimeout(() => {
       runAnimation(solution, i + 1, jug1, jug2, aim);
-    }, 500); 
+    }, 500);
   }
+}
 
 // display output
 function displayOutput2(solution: JugSolution) {
